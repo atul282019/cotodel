3aa1ec049008edf20fe713e4a4db7e0549201d87
atulyadavayo <atulyadavayo@gmail.com>
2024-02-20 16:08:36 +0530

    Revert "update changes 20-02-2024"
    This reverts commit cc44cdcf18ce0e70014d1962cf19f951554fb9d8.
    
    sh: line 1: wq: command not found
    h: line 1: q: command not found
    sh: line 1: q: command not found
    sh: line 1: wq: command not found

--- src/main/resources/static/cotodel-js/company-detail.js
+++ src/main/resources/static/cotodel-js/company-detail.js
@@ -291,25 +291,28 @@ function validateFormAndSubmit() {
 			var data1 = jQuery.parseJSON(newData);
 			console.log(data1)
 			if(data1.status==true){
-				 document.getElementById("successmsg").innerHTML=data1.message;
+				 document.getElementById("successmsg").innerHTML="Data Saved Successfully";
 				 document.getElementById("successmsgdiv").style.display="block";
 				 //document.getElementById("saveorg").reset();
-				// $('#successmsgdiv').delay(5000).fadeOut(4000);
+				 $('#successmsgdiv').delay(5000).fadeOut(400);
 				// $("#form5").show();
 				document.getElementById("saveNext").style.display="None";
 				//document.getElementById("nextDiv").style.display="flex";
-				$('#nextDiv').show('slow');
-			
+				//$('#nextDiv').show('slow');
+				$("#form4").hide();
+                $("#form5").show();
+                $("#tab2").addClass("active");
+				
                 // $("#form4").hide();
 			}else if(data1.status==false){
 				 document.getElementById("failmsg").innerHTML=data1.message;
 				 document.getElementById("failmsgDiv").style.display="block";
-				 //$('#failmsgDiv').delay(5000).fadeOut(4000);
+				 $('#failmsgDiv').delay(5000).fadeOut(400);
 				 //$("#form5").show();
                  //$("#form4").hide();
                  document.getElementById("saveNext").style.display="block";
 				 document.getElementById("nextDiv").style.display="None";
-				  $("#nextDiv").hide();
+				 $('#nextDiv').show('hide');
 			}else{
 				 document.getElementById("failmsgDiv").style.display="none";
 				 document.getElementById("successmsgdiv").style.display="none";
--- target/classes/static/cotodel-js/masterdata.js
+++ target/classes/static/cotodel-js/masterdata.js
@@ -67,39 +67,3 @@ function getOrgMaster() {
          }
     });
 } 
-
-
-function getOrgMaster2() {
-  $.ajax({
-		type: "GET",
-        url: ""+$('#ctx').attr('content')+"/getOrgMaster",
-           success: function(data){
-            newData = data;
-//            console.log(newData);
-			$("#orgType2 option").remove();
-            var obj = jQuery.parseJSON( data );
-             obj = obj.data;
-        	 var count=0;
-         	for (var key in obj) {
-
-             var values =  obj[key];
-             var x = document.getElementById("orgType2");
-             if(count==0){
-             var option = document.createElement("option");
-             option.text ="Select OrgType";
-             option.value = "";
-             x.add(option);
-             }
-             var option = document.createElement("option");
-             option.text = values.org_type;
-             option.value = values.org_type;
-             x.add(option);
-
-             count++;
-             }   
-         },
-         error: function(e){
-             alert('Error: ' + e);
-         }
-    });
-} 
